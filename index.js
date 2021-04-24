bc0b6400596271f2557276c146014110a144c9f2
SuperPox <calebkohl@gmail.com>
2021-04-23 23:50:44 -0600

    commit tree 6799addb3a6c64058ff5c3318880736ee686eb68

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers)
+{
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (drivers)
+{
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiply)
+{
+    return function (result)
+    {
+        return multiply * result;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (d1, d2) {return d2(d1)}
